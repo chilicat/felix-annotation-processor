91b9b96f505d48ca6cf3ec44651bfa92c93a7e64
Daniel Kuffner <dkuffner@chilicat.net>
2013-08-05 15:18:12 +0200

    windows path handling bugfix

--- src/net/chilicat/felixscr/intellij/build/scr/ScrSource.java
+++ src/net/chilicat/felixscr/intellij/build/scr/ScrSource.java
@@ -1,13 +1,19 @@
 package net.chilicat.felixscr.intellij.build.scr;
 
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.apache.felix.scrplugin.Source;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.logging.Logger;
 
 class ScrSource implements Source {
+
+    private final static Logger logger = Logger.getLogger(ScrSource.class.getName());
+
     private final File f;
     private final VirtualFile[] sourceRoots;
     private String extension;
@@ -27,17 +33,31 @@ class ScrSource implements Source {
     }
 
     private String toClassName(File f, VirtualFile[] sourceRoots) {
-        String path = f.getAbsolutePath();
-
-        if (path.endsWith(extension)) {
-            for (VirtualFile root : sourceRoots) {
-                if (path.startsWith(root.getPath())) {
-                    String cls = path.substring(root.getPath().length() + 1, path.length() - extension.length());
-                    return cls.replace(File.separator, ".");
+        try {
+            String path = normalizePath(f.getCanonicalPath());
+            if (path.endsWith(extension)) {
+                for (VirtualFile root : sourceRoots) {
+                    String rootPath = normalizePath(root.getCanonicalPath());
+                    if (startsWithIgnoreCase(path, rootPath)) {
+                        String cls = path.substring(rootPath.length() + 1, path.length() - extension.length());
+                        return cls.replace('/', '.');
+                    }
                 }
             }
+
+            throw new RuntimeException("Cannot find source root for " + f.getAbsolutePath());
+
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot resolve canonical path: " + f.getAbsolutePath() + " Message: " + e.getMessage());
         }
-        throw new RuntimeException("Cannot find source root for " + f.getAbsolutePath());
+    }
+
+    private boolean startsWithIgnoreCase(String path, String rootPath) {
+        return path.toLowerCase().startsWith(rootPath.toLowerCase());
+    }
+
+    private String normalizePath(String path) {
+        return path.replace(File.separatorChar, '/');
     }
 
     @Override
